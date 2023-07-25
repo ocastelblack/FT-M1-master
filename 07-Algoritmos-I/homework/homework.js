1620207dd23e18b5990c85593489d2f27bbaea60
ocastelblack <oscard.castelblanco@gmail.com>
2023-07-25 18:27:29 -0500

    homework7

--- 07-Algoritmos-I/homework/homework.js
+++ 07-Algoritmos-I/homework/homework.js
@@ -6,15 +6,47 @@ function factorear(num) {
   // los factores por los cuales se va dividiendo a dicho número (De menor a mayor)
   // Ej: factorear(180) --> [1, 2, 2, 3, 3, 5] Ya que 1x2x2x3x3x5 = 180 y son todos números primos
   // Tu código:
+  const factores = [1];
+  let divisor = 2;
 
+  while (num > 1) {
+    if (num % divisor === 0) {
+      factores.push(divisor);
+      num /= divisor;
+    } else {
+      divisor++;
+    }
+  }
+
+  return factores;
 }
+console.log(factorear(180)); // Imprimirá el resultado de factorear(180) en la consola
+console.log(factorear(32));  // Imprimirá el resultado de factorear(32) en la consola
+console.log(factorear(33));  // Imprimirá el resultado de factorear(33) en la consola
+console.log(factorear(1));
 
 function bubbleSort(array) {
   // Implementar el método conocido como bubbleSort para ordenar de menor a mayor
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
+  const length = array.length;
+  let swapped;
 
+  do {
+    swapped = false;
+    for (let i = 0; i < length - 1; i++) {
+      if (array[i] > array[i + 1]) {
+        // Realizar el intercambio
+        const temp = array[i];
+        array[i] = array[i + 1];
+        array[i + 1] = temp;
+        swapped = true;
+      }
+    }
+  } while (swapped);
+
+  return array;
 }
 
 
@@ -23,7 +55,21 @@ function insertionSort(array) {
   // el array recibido como parámetro utilizando arreglos
   // Devolver el array ordenado resultante
   // Tu código:
+  const length = array.length;
 
+  for (let i = 1; i < length; i++) {
+    const currentElement = array[i];
+    let j = i - 1;
+
+    while (j >= 0 && array[j] > currentElement) {
+      array[j + 1] = array[j];
+      j--;
+    }
+
+    array[j + 1] = currentElement;
+  }
+
+  return array;
 }
 
 
@@ -32,7 +78,26 @@ function selectionSort(array) {
   // el array recibido como parámetro utilizando dos arreglos
   // Devolver el array ordenado resultante
   // Tu código:
+  const length = array.length;
 
+  for (let i = 0; i < length - 1; i++) {
+    let minIndex = i;
+
+    for (let j = i + 1; j < length; j++) {
+      if (array[j] < array[minIndex]) {
+        minIndex = j;
+      }
+    }
+
+    if (minIndex !== i) {
+      // Intercambiar elementos
+      const temp = array[i];
+      array[i] = array[minIndex];
+      array[minIndex] = temp;
+    }
+  }
+
+  return array;
 }
 
 
